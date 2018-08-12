9f00e199f44dacfd04e0235804fa05a997b9fd6f
Andrew Thyng <andrew.thyng@gmail.com>
2018-08-11 20:10:37 -0500

    Fix up server to host the bundled site

--- server.js
+++ server.js
@@ -2,8 +2,21 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+app.use(
+  '/static/js',
+  express.static(path.join(__dirname, 'build', 'static', 'js'))
+)
+
+app.use(
+  '/audio/marimba',
+  express.static(path.join(__dirname, 'build', 'audio', 'marimba'))
+)
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'))
+  const options = {
+    root: __dirname + '/build/'
+  }
+  res.sendFile('index.html', options)
 })
 
 app.listen(8080, () => console.log('Listening'))
