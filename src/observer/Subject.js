34556f64d73628d5852b1fdcb0001715b3899f3c
ajt79 <ajt79@txstate.edu>
2019-11-19 14:50:59 -0600

    Fixed arguments keyword

--- src/observer/Subject.js
+++ src/observer/Subject.js
@@ -1,24 +1,15 @@
 const _handlers = {}
 
-const checkArgs = (actual, expected) => {
-  if (actual !== expected) {
-    throw new TypeError(`expected 2 arguments and received ${arguments.length}`)
-  }
-}
 
 const Subject = {
-  subscribe () {
-    checkArgs(arguments.length, 2)
-
-    const [event, handler] = arguments
+  subscribe (...args) {
+    const [event, handler] = args
 
     if (!_handlers[event]) _handlers[event] = []
     _handlers[event].push(handler)
   },
-  unsubscribe () {
-    checkArgs(arguments.length, 2)
-
-    const [event, handler] = arguments
+  unsubscribe (...args) {
+    const [event, handler] = args
 
     if (!_handlers[event]) return
     _handlers[event] = _handlers[event].filter(func => func !== handler)
